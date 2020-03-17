54ed4b9bd897634e0635af0ea6f851112c8dd586
potential20 <passion212@nate.com>
2020-03-17 12:43:08 +0900

    롱클릭 이벤트 별도 처리

--- app/src/main/java/kr/co/tjoeun/a20200316_03_uipractice_test/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200316_03_uipractice_test/MainActivity.java
@@ -3,12 +3,58 @@ package kr.co.tjoeun.a20200316_03_uipractice_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn = null;
+    TextView findPwTxt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        findPwTxt = findViewById(R.id.findPwTxt);
+
+//        로그인버튼의 글자를 => 회원가입으로 바꾸자.
+        loginBtn.setText("회원가입");
+
+//        비밀번호 찾기 텍스트뷰를 => JAVA에서 "비번 찾기" 로 변경.
+//        findPwTxt 로 id를 부여하자.
+
+        findPwTxt.setText("비번 찾기");
+
+//        로그인 버튼이 눌리면 => 비번 찾기 버튼을 "AAA"로 변경
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                findPwTxt.setText("AAA");
+            }
+        });
+
+//        클릭은 버튼 뿐 아니라 모든 뷰가 다 늘릴 수 있다. => 텍스트뷰도 클릭.
+
+        findPwTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPwTxt.setText("비밀번호 찾기");
+            }
+        });
+
+        loginBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                loginBtn.setText("로그인");
+                return true; // 손을 뗐을때, onClick실행을 막을건지?
+            }
+        });
+
     }
 }
+
--- app/src/main/java/kr/co/tjoeun/a20200316_03_uipractice_test/SignUpActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200316_03_uipractice_test/SignUpActivity.java
@@ -3,12 +3,20 @@ package kr.co.tjoeun.a20200316_03_uipractice_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import kr.co.tjoeun.a20200316_03_uipractice_test.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+
     }
 }
