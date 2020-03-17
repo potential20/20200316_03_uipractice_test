720a71965f6468fbcbe243ca09b346fc9a7f1625
potential20 <passion212@nate.com>
2020-03-17 09:36:06 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200316_03_uipractice_test/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.a20200316_03_uipractice_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
